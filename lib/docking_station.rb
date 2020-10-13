8449cca6b272ea5bbe93f6b7a262d65620b95cbd
swizel <swizel.sarah@gmail.com>
2020-10-13 17:36:05 +0100

    14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,21 @@
+require './lib/bike.rb'
 class DockingStation
 
-def release_bike
-end
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+    @capacity = 20
+  end
+
+  def release_bike
+    raise "There are no bikes available." unless @bikes.length > 0
+    @bikes.pop
+  end
+
+  def dock(bike)
+    raise "Docking station full." unless @bikes.length < @capacity
+    @bikes << bike
+    return @bikes.last
+  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,35 @@ require 'docking_station.rb'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  describe '#release_bike' do
+    it 'release working bikes' do
+      #make bike
+      bike = Bike.new
+      #dock bikee
+      subject.dock(bike)
+      #release docked bike
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  it 'docks a bike' do
+    is_expected.to respond_to(:dock).with(1).argument
+  end
+
+  it 'docks working bikes' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'raises error when no more bike' do
+    expect { subject.release_bike }.to raise_error "There are no bikes available."
+  end
+
+  it 'raises error dock is full' do
+    20.times {
+    subject.dock(Bike.new) }
+    expect { subject.dock(Bike.new) }.to raise_error "Docking station full."
+  end
+
 end
